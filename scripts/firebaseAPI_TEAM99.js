dea4947f098117b4c324c20e1a12fa605da1550c
Balpreet Sekhon <balpreetsekhon787@gmail.com>
2024-02-20 10:13:39 -0800

    Finish demo07

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyDamVotzrIlPjLOvKdPZhd23zqubahUBwQ",
+    authDomain: "comp1800-c51af.firebaseapp.com",
+    projectId: "comp1800-c51af",
+    storageBucket: "comp1800-c51af.appspot.com",
+    messagingSenderId: "298421369476",
+    appId: "1:298421369476:web:b0a90465f2a1c1b2d720d1"
 };
 
 //--------------------------------------------
